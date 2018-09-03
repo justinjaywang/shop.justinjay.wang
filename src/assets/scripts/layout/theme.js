1a801a637d6efc50fc933754fbc684212438339c
Justin Wang <justinjaywang@gmail.com>
2018-09-03 10:20:23 -0700

    add flickity

--- src/assets/scripts/layout/theme.js
+++ src/assets/scripts/layout/theme.js
@@ -1,2 +1,22 @@
 import "../../styles/theme.scss";
 import "../../styles/theme.scss.liquid";
+
+import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
+import {cookiesEnabled} from '@shopify/theme-cart';
+
+// Common a11y fixes
+focusHash();
+bindInPageLinks();
+
+// Apply a specific class to the html element for browser support of cookies.
+if (cookiesEnabled()) {
+  document.documentElement.className = document.documentElement.className.replace(
+    'supports-no-cookies',
+    'supports-cookies',
+  );
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.add('js');
+  // initLazyLoad(); // initialize lazy load
+});
